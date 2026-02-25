8960d56b1c5ecc1a719a12e4e5ee85bd1e09fac5
Rahma Nader <r.nader@egac.rak.ae>
2026-02-25 07:42:08 +0200

    added back buttons

---
+++ test.js
@@ -0,0 +1,13 @@
+//excersice 1
+console.log("excersice 1");
+let a = 20;
+let b = 5;
+console.log(a > b ? "a is bigger" : "b is bigger" );
+
+function max (a, b){
+    return a > b ? "a is bigger" : "b is bigger"
+}
+console.log(max(6,7));
+
+//excersice 2
+console.log("excercise 3");
